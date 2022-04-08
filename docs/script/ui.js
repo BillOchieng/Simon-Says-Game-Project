cb79c49fdad415d3232066d8a7e95cc636a60161
BillOchieng <ochieng01@allegheny.edu>
2022-04-08 15:39:59 -0400

    initial push

--- docs/script/ui.js
+++ docs/script/ui.js
@@ -1,1 +1,23 @@
+var gameBtns = document.querySelectorAll(".game-button");
+var userChoices = []
+
+
+var startBtn = document.getElementById("start-button");
+userChoices = [];
+const elements = Array.from(gameBtns).map(button => {
+    return button.id;
+});
+//generate a random number index inf from 0-3
+let choice = math.floor(math.random() * elements.length)
+// get IDs of all possible buttons
+//console.log(choice);
+//get IDs for a specific possible number
+//console.log(elements[choice]);
+
+startBtn.addEventListener("click", () => {
+    document.getElementById("points").innerText = "Points: 0";
+    startBtn.style.backgroundColor = "#FF3898";
+    console.log("MADE IT HERE");
+});
 // TODO: Leave this alone until prompted!
+
